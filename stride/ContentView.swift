046fc68fa93019992ad634b786b03788a1f29ed6
abbiesdepot <cabigail03@student.ciputra.ac.id>
2026-06-02 15:33:07 +0700

    Initial Commit

---
+++ stride/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stride
+//
+//  Created by abbie on 02/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stride/strideApp.swift
@@ -0,0 +1,17 @@
+//
+//  strideApp.swift
+//  stride
+//
+//  Created by abbie on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct strideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
